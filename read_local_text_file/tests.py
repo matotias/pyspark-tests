a043f29457a872dde49c885a38e2fdc0f15f2d2b
Matias Galleguillos <matotias@gmail.com>
2019-09-05 12:34:22 -0400

    Add more textfile tests

--- read_local_text_file/tests.py
+++ read_local_text_file/tests.py
@@ -6,6 +6,12 @@ text_file = sc.textFile('exampleFile.txt')
 print(text_file.count())
 print(text_file.first())
 
-SECFIND = text_file.filter(lambda line: 'second' in line)
-print(SECFIND.collect())
-print(SECFIND.count())
+SEC_FIND = text_file.filter(lambda line: 'second' in line)
+print(SEC_FIND.collect())  # collect -> turn RDD to in memory list
+print(SEC_FIND.count())
+
+print(text_file.collect())
+print(text_file.map(lambda x: x.split()).collect())
+print(text_file.flatMap(lambda x: x.split()).collect())
+
+print(text_file.union(text_file.flatMap(lambda x: x.split())).collect())
