4611cd4553fa22c318ab6854b5bf8a7bf73048ca
Matias Galleguillos <matotias@gmail.com>
2019-09-11 18:00:07 -0300

    extract data with a subquery

--- read_from_postgresql/dbtests.py
+++ read_from_postgresql/dbtests.py
@@ -1,7 +1,7 @@
 from json import loads
 from pyspark.sql import SparkSession
 
-config = loads(open('./config.json').read())
+config = loads(open('./config.json').read())['db']
 url = "jdbc:{adapter}://{host}:{port}/{database}?user={username}&password={password}".format(**config)
 
 spark = SparkSession\
@@ -10,14 +10,27 @@ spark = SparkSession\
     .config('spark.jars', './postgresql-42.2.6.jar')\
     .getOrCreate()
 
-df = spark\
+query = '(select count(*) as a from test.sales) as a'
+
+sales = spark\
     .read\
     .format('jdbc')\
     .options(
         url=url,
         database=config['database'],
-        dbtable='test.sales',\
+        dbtable=query,
         driver='org.postgresql.Driver')\
     .load()
 
-print(df.printSchema())
+output = sales
+
+output\
+    .write\
+    .format('jdbc')\
+    .options(
+        url=url,
+        database=config['database'],
+        dbtable='test.pyspark_tests',
+        driver='org.postgresql.Driver')\
+    .mode('append')\
+    .save()
